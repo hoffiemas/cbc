24a672f6d49ad07d29226699348c8e7c757e953b
hoffiemas <hoffie_mas@hotmail.com>
2017-08-17 13:39:14 +0200

    Create oecd_types_2016.ts

---
+++ oecd_types_2016.ts
@@ -0,0 +1,10 @@
+const DocTypeIndic_2016= [
+       // { value: "OECD0", name: "Resent Data" }, not used
+        { value: "OECD1", name: "New Data" },
+        { value: "OECD2", name: "Corrected Data" },
+        { value: "OECD3", name: "Deletion of Data" },
+      //  { value: "OECD10", name: "Resent Test Data" }, not used
+        { value: "OECD11", name: "New Test Data" },
+        { value: "OECD12", name: "Corrected Test Data" },
+        { value: "OECD13", name: "Deletion of Test Data" }
+      ]
